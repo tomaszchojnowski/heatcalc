d7a5d332a7fb53b437916798fabdeb84661b8553
Tom Chojnowski <56504926+tomaszchojnowski@users.noreply.github.com>
2026-01-31 20:15:51 +0000

    rotation try

--- ModelViewer3D.js
+++ ModelViewer3D.js
@@ -396,7 +396,7 @@ export class ModelViewer3D {
     
     const roofMesh = new THREE.Mesh(geometry, material);
     roofMesh.position.set(dims.width / 2, roofLevel, 0);
-    roofMesh.rotation.x = Math.PI / 2;
+    roofMesh.rotation.z = Math.PI / 2;
     roofMesh.castShadow = true;
     roofMesh.receiveShadow = true;
     roofMesh.userData = { 
